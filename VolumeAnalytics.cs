8a6e4d3a90215052882a7e0b541ea00d55a73b6a
Yusuf Wadud <ywadud94@gmail.com>
2017-10-24 05:34:35 -0400

    Add files via upload

--- VolumeAnalytics.cs
+++ VolumeAnalytics.cs
@@ -28,25 +28,26 @@ namespace NinjaTrader.NinjaScript.Indicators
 {
 	public class VolumeAnalytics : Indicator
 	{
-		private decimal ask = 0;
-		private decimal bid = 0;
+		private decimal ask = 0m;
+		private decimal bid = 0m;
 		private decimal buySellRatio;
 		private decimal sigRatio = (decimal)(10/7);
+		private decimal zeroRatio = 0.15m;
 		
 		private int activeBar = 0;
-		private decimal lastOpen = 0;
-		private decimal lastClose = 0;
-		private decimal lastHigh = 0;
-		private decimal lastLow = 0;
+		private decimal lastOpen = 0m;
+		private decimal lastClose = 0m;
+		private decimal lastHigh = 0m;
+		private decimal lastLow = 0m;
 		private int lastVol = 0;
 		
-		private decimal currOpen = 0;
-		private decimal currClose = 0;
-		private decimal currHigh = 0;
-		private decimal currLow = 0;
+		private decimal currOpen = 0m;
+		private decimal currClose = 0m;
+		private decimal currHigh = 0m;
+		private decimal currLow = 0m;
 		private int currVol = 0;
 		private int currIndex = 0;
-		private decimal ticker = 0;
+		private decimal ticker = 0m;
 		
 		private int delayCount = 5;
 		private int delay = 5;
@@ -111,6 +112,8 @@ namespace NinjaTrader.NinjaScript.Indicators
 				priceLevels.Add((decimal)Close[0]);
 				volumes.Add((int)Volume[0]);
 				
+				Print("\nDebug 1");
+				
 				if (lastClose >= ask)
 				{
 					buyVols.Add(0);
@@ -121,15 +124,25 @@ namespace NinjaTrader.NinjaScript.Indicators
 					buyVols.Add(lastVol);
 					sellVols.Add(0);
 				}
+				else
+				{
+					buyVols.Add(0);
+					sellVols.Add(0);
+				}
 			}
 			else
 			{
+				Print("\nDebug 2");
 				updateCurrent();
 				
+				Print("\nDebug 3");
 				checkAndUpdate();
+				Print("\nDebug 4");
 				checkForGap();
+				Print("\nDebug 5");
 				findPOCAndCluster();
 				
+				Print("\nDebug 6");
 				updateLast();
 			}
 		}
@@ -163,20 +176,29 @@ namespace NinjaTrader.NinjaScript.Indicators
 			{
 				// Prices will be listed high to low
 				// Check where Close[0] is in the list for an update to volume or if it needs to be inserted as a new price point 
+				Print("\nDebug 7");
 				if(currClose > priceLevels[i]) 
 				{
 					// Insert
+					Print("\nDebug 8");
 					priceLevels.Insert(i, currClose);
 					volumes.Insert(i, currVol - lastVol);
 					if (lastClose >= ask)
 					{
-						buyVols.Insert(i, 0);
-						sellVols.Insert(i, currVol - lastVol);
+						Print("\nDebug 9");
+						buyVols.Insert(i, currVol - lastVol);
+						sellVols.Insert(i, 0);
 					}
 					else if (lastClose <= bid)
 					{
-						buyVols.Insert(i, currVol - lastVol);
-						sellVols.Insert(i, 0);
+						Print("\nDebug 10");
+						buyVols.Insert(i, 0);
+						sellVols.Insert(i, currVol - lastVol);
+					}
+					else
+					{
+						buyVols.Add(0);
+						sellVols.Add(0);
 					}
 					
 					break;
@@ -184,22 +206,28 @@ namespace NinjaTrader.NinjaScript.Indicators
 				else if (currClose == priceLevels[i]) 
 				{
 					// Update
+					Print("\nDebug 11");
 					volumes[i] += (int)currVol - lastVol;
+					Print("\nDebug 11B");
 					if (lastClose >= ask)
 					{
-						sellVols[i] += (int)currVol - lastVol;
+						Print("\nDebug 11C");
+						buyVols[i] = buyVols[i] + currVol - lastVol;
 					}
 					else if (lastClose <= bid)
 					{
-						buyVols[i] += (int)currVol - lastVol;
+						Print("\nDebug 11D");
+						sellVols[i] = sellVols[i] + currVol - lastVol;
 					}
 					break;
 				}
 				else if (currClose < priceLevels[i])
 				{
+					Print("\nDebug 12");
 					if (priceLevels.Count == (i + 1))
 					{
 						// Add if you're already at the end of the list
+						Print("\nDebug 13");
 						priceLevels.Add(currClose); 
 						volumes.Add(currVol); 
 						if (lastClose >= ask)
@@ -212,20 +240,33 @@ namespace NinjaTrader.NinjaScript.Indicators
 							buyVols.Add(currVol);
 							sellVols.Add(0);
 						}
+						else
+						{
+							buyVols.Add(0);
+							sellVols.Add(0);
+						}
 					}
 					else
 					{
 						// Insert 
+						Print("\nDebug 14");
 						priceLevels.Insert(i + 1, currClose);
 						volumes.Insert(i + 1, currVol - lastVol);
+						
+						Print("\nDebug 15");
 						if (lastClose >= ask)
+						{
+							buyVols.Insert(i + 1, currVol - lastVol);
+							sellVols.Insert(i + 1, 0);
+						}
+						else if (lastClose <= bid)
 						{
 							buyVols.Insert(i + 1, 0);
 							sellVols.Insert(i + 1, currVol - lastVol);
 						}
-						else if (lastClose <= bid)
+						else
 						{
-							buyVols.Insert(i + 1, currVol - lastVol);
+							buyVols.Insert(i + 1, 0);
 							sellVols.Insert(i + 1, 0);
 						}
 					}
@@ -237,30 +278,38 @@ namespace NinjaTrader.NinjaScript.Indicators
 		
 		private void checkForGap() 
 		{
+			Print("\nDebug 16");
 			for (int i = 0; i < (priceLevels.Count - 1); i++)
 			{
-				while ((priceLevels[i] - priceLevels[i + 1]) > ticker)
+				if ((i + 1) < priceLevels.Count)
 				{
-					priceLevels.Insert(i + 1, priceLevels[i] - ticker);
-					volumes.Insert(i + 1, 0);
-					buyVols.Insert(i + 1, 0);
-					sellVols.Insert(i + 1, 0);
+					Print("\nDebug 17");
+					while ((priceLevels[i] - priceLevels[i + 1]) > ticker)
+					{
+						priceLevels.Insert(i + 1, priceLevels[i] - ticker);
+						volumes.Insert(i + 1, 0);
+						buyVols.Insert(i + 1, 0);
+						sellVols.Insert(i + 1, 0);
+					}
 				}
 			}
 		}
 		
 		private void findPOCAndCluster()
 		{
-			decimal tempClusterTop = 0;
-			decimal highVolPrice = 0;
+			decimal tempClusterTop = 0m;
+			decimal highVolPrice = 0m;
 			int highestVol = 0;
 			int clusterVol = 0;
 			
+			Print("\nDebug 18");
 			for (int i = 0; i < priceLevels.Count; i++)
 			{
 				// Find Clusters
+				Print("\nDebug 19");
 				if (priceLevels.Count > 5)
 				{
+					Print("\nDebug 20");
 					if ((i+2) < priceLevels.Count)
 					{
 						if (clusterVol < (volumes[i] + volumes[i + 1] + volumes[i + 2]))
@@ -269,39 +318,112 @@ namespace NinjaTrader.NinjaScript.Indicators
 							tempClusterTop = priceLevels[i];
 						}
 					}
-//					if (i < (priceLevels.Count - 2))
+					
+//					if (i < (priceLevels.Count - 3))
 //					{
-//						if ((buyVols[i + 1] / sellVols[i]) >= sigRatio && (buyVols[i + 2] / sellVols[i + 1]) >= sigRatio & (buyVols[i + 3] / sellVols[i + 2]) >= sigRatio)
+//						Print("\nDebug 21");
+//						bool highBuy = true;
+//						bool highSell = true;
+						
+//						for (int j = 1; j < 4; j++)
 //						{
-//							// Draw a blue dot or arrow from buyVols[i + 1] and sellVols[i + 1]) to buyVols[i + 2] / sellVols[i + 2]
-//							string strTag = "Bar" + CurrentBar + "Price" + priceLevels[i + 1]; 
-//							Draw.Dot(this, strTag, true, 0, (double)(buyVols[i + 1] + sellVols[i + 1])/2, Brushes.Blue, true);
+//							if (sellVols[i + j - 1] > 0 && (decimal)(buyVols[i + j] / sellVols[i + j - 1]) >= sigRatio)
+//								continue;
+//							else if (sellVols[i + j - 1] <= 0  && buyVols[i + j] >= (zeroRatio*(decimal)currVol))
+//								continue;
+//							else
+//							{
+//								highBuy = false;
+//								break;
+//							}
 //						}
-//						else if ((sellVols[i] / buyVols[i + 1]) >= sigRatio && (sellVols[i + 1] / buyVols[i + 2]) >= sigRatio & (sellVols[i + 2] / buyVols[i + 3]) >= sigRatio)
+						
+//						Print("\nDebug 22");
+//						if (!highBuy)
+//						{
+//							for (int j = 1; j < 4; j++)
+//							{
+//								if (buyVols[i + j] > 0m && (decimal)(sellVols[i + j - 1] / buyVols[i + j]) >= sigRatio)
+//									continue;
+//								else if (buyVols[i + j] <= 0m  && sellVols[i + j - 1] >= (zeroRatio*(decimal)currVol))
+//									continue;
+//								else 
+//								{
+//									highSell = false;
+//									break;
+//								}
+//							}
+//						}
+						
+//						Print("\nDebug 23");
+//						if (highBuy)
 //						{
 //							// Draw a white or purple dot or arrow from buyVols[i + 1] and sellVols[i + 1]) to buyVols[i + 2] / sellVols[i + 2]
 //							string strTag = "Bar" + CurrentBar + "Price" + priceLevels[i + 1]; 
-//							Draw.Dot(this, strTag, true, 0, (double)(buyVols[i + 1] + sellVols[i + 1])/2, Brushes.Purple, true);
+//							Draw.Dot(this, strTag, true, 0, (double)buyVols[i + 2], Brushes.Blue, true);
+//						}
+//						else if (highSell)
+//						{
+//							// Draw a white or purple dot or arrow from buyVols[i + 1] and sellVols[i + 1]) to buyVols[i + 2] / sellVols[i + 2]
+//							string strTag = "Bar" + CurrentBar + "Price" + priceLevels[i + 1]; 
+//							Draw.Dot(this, strTag, true, 0, (double)sellVols[i + 1], Brushes.Purple, true);
 //						}
 //					}
 				}
-//				else if (priceLevels.Count > 3 && i < (priceLevels.Count - 2))
+//				else if (priceLevels.Count > 3 && i < (priceLevels.Count - 3))
 //				{
-//					if ((buyVols[i + 1] / sellVols[i]) >= sigRatio && (buyVols[i + 2] / sellVols[i + 1]) >= sigRatio & (buyVols[i + 3] / sellVols[i + 2]) >= sigRatio)
+				
+//					Print("\nDebug 24");
+//					bool highBuy = true;
+//					bool highSell = true;
+					
+//					for (int j = 1; j < 4; j++)
 //					{
-//						// Draw a blue dot or arrow from buyVols[i + 1] and sellVols[i + 1]) to buyVols[i + 2] / sellVols[i + 2]
-//						string strTag = "Bar" + CurrentBar + "Price" + priceLevels[i + 1]; 
-//						Draw.Dot(this, strTag, true, 0, (double)(buyVols[i + 1] + sellVols[i + 1])/2, Brushes.Blue, true);
+//						if (sellVols[i + j - 1] > 0m && (decimal)(buyVols[i + j] / sellVols[i + j - 1]) >= sigRatio)
+//							continue;
+//						else if (sellVols[i + j - 1] <= 0m  && buyVols[i + j] >= (zeroRatio*(decimal)currVol))
+//							continue;
+//						else
+//						{
+//							highBuy = false;
+//							break;
+//						}
 //					}
-//					else if ((sellVols[i] / buyVols[i + 1]) >= sigRatio && (sellVols[i + 1] / buyVols[i + 2]) >= sigRatio & (sellVols[i + 2] / buyVols[i + 3]) >= sigRatio)
+					
+//					Print("\nDebug 25");
+//					if (!highBuy)
+//					{
+//						for (int j = 1; j < 4; j++)
+//						{
+//							if ( buyVols[i + j] > 0m && (decimal)(sellVols[i + j - 1] / buyVols[i + j]) >= sigRatio)
+//								continue;
+//							else if (buyVols[i + j] <= 0m  && sellVols[i + j - 1] >= (zeroRatio*(decimal)currVol))
+//								continue;
+//							else 
+//							{
+//								highSell = false;
+//								break;
+//							}
+//						}
+//					}
+					
+//					Print("\nDebug 26");
+//					if (highBuy)
 //					{
 //						// Draw a white or purple dot or arrow from buyVols[i + 1] and sellVols[i + 1]) to buyVols[i + 2] / sellVols[i + 2]
 //						string strTag = "Bar" + CurrentBar + "Price" + priceLevels[i + 1]; 
-//						Draw.Dot(this, strTag, true, 0, (double)(buyVols[i + 1] + sellVols[i + 1])/2, Brushes.Purple, true);
+//						Draw.Dot(this, strTag, true, 0, (double)buyVols[i + 2], Brushes.Blue, true);
+//					}
+//					else if (highSell)
+//					{
+//						// Draw a white or purple dot or arrow from buyVols[i + 1] and sellVols[i + 1]) to buyVols[i + 2] / sellVols[i + 2]
+//						string strTag = "Bar" + CurrentBar + "Price" + priceLevels[i + 1]; 
+//						Draw.Dot(this, strTag, true, 0, (double)sellVols[i + 1], Brushes.Purple, true);
 //					}
 //				}
 				
 				// Find POC
+				Print("\nDebug 27");
 				if(highestVol < volumes[i])
 				{
 					highVolPrice = priceLevels[i];
@@ -309,23 +431,25 @@ namespace NinjaTrader.NinjaScript.Indicators
 				}
 				
 				// Outputs for debugging
-				Print("\n\nPrice: ");
-				Print(priceLevels[i]);
-				Print("\nVolume:");
-				Print(volumes[i]);
-				Print("Highest Vol Price: ");
-				Print(highVolPrice);
-				Print("Highest Vol: ");
-				Print(highestVol);
+//				Print("\n\nPrice: ");
+//				Print(priceLevels[i]);
+//				Print("\nVolume:");
+//				Print(volumes[i]);
+//				Print("Highest Vol Price: ");
+//				Print(highVolPrice);
+//				Print("Highest Vol: ");
+//				Print(highestVol);
 			} 
 			
 			if (priceLevels.Count > 5)
 			{
+				Print("\nDebug 28");
 				Values[0][0] = ((double)tempClusterTop); 
 				Values[1][0] = (double)(tempClusterTop - ticker); 
 				Values[2][0] = (double)(tempClusterTop - ticker - ticker); 
 			}
 			
+			Print("\nDebug 29");
 			Values[3][0] = ((double)highVolPrice); 
 		}
 
